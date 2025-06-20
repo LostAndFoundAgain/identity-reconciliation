50ff9988c1497d27e901c885a7eee8e6c033f17a
Nikhil Upadhyay <nikrkt1.com>
2025-06-20 21:34:53 +0530

    Add identity entity schema

--- src/identity/entities/identity.entity.ts
+++ src/identity/entities/identity.entity.ts
@@ -1,1 +1,35 @@
-export class Identity {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Identity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  email: string;
+
+  @Column({ nullable: true })
+  linkedId: number;
+
+  @Column({ default: "primary" })
+  linkPrecedence: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
