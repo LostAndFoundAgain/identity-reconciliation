afab9f774f17fe453c3b0e08fb2011ddc98d2cec
Nikhil Upadhyay <nikrkt1.com>
2025-06-21 11:12:31 +0530

    process return data into API response

--- src/identity/identity.service.ts
+++ src/identity/identity.service.ts
@@ -3,6 +3,10 @@ import { CreateIdentityDto } from "./dto/create-identity.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Identity } from "./entities/identity.entity";
 import { Repository } from "typeorm";
+import {
+  ContactDto,
+  CreateIdentityResponseDto,
+} from "./dto/create-identity-response.dto";
 
 @Injectable()
 export class IdentityService {
@@ -12,23 +16,46 @@ export class IdentityService {
   ) {}
 
   async create(createIdentityDto: CreateIdentityDto) {
-    const primaryContactExists = await this.findOne(
+    const existingContacts = await this.find(
       createIdentityDto.email,
       createIdentityDto.phoneNumber
     );
     const contactToSave = { ...createIdentityDto } as Identity;
 
-    if (primaryContactExists) {
+    if (existingContacts) {
       contactToSave.linkPrecedence = "secondary";
-      contactToSave.linkedId = primaryContactExists.id;
+      contactToSave.linkedId = existingContacts[0].id;
     }
 
-    return this.identityRepository.save(contactToSave);
+    const savedNewContact = await this.identityRepository.save(contactToSave);
+    existingContacts.push(savedNewContact);
+    return this.createContactResponse(existingContacts);
   }
 
-  findOne(email: string, phoneNumber: string): Promise<Identity> {
-    return this.identityRepository.findOne({
+  find(email: string, phoneNumber: string): Promise<Identity[]> {
+    return this.identityRepository.find({
       where: [{ email }, { phoneNumber }],
     });
+  }
+
+  createContactResponse(existingContacts: Identity[]) {
+    const createIdentityResponseDto = new CreateIdentityResponseDto();
+    const contactDetails = new ContactDto();
+
+    contactDetails.primaryContactId = existingContacts[0].id;
+
+    for (const existingContact of existingContacts) {
+      if (existingContact.linkPrecedence === "primary") {
+        contactDetails.primaryContactId = existingContact.id;
+      } else {
+        contactDetails.secondaryContactIds.push(existingContact.id);
+      }
+
+      contactDetails.emails.push(existingContact.email);
+      contactDetails.phoneNumbers.push(existingContact.phoneNumber);
+    }
+
+    createIdentityResponseDto.contact = contactDetails;
+    return createIdentityResponseDto;
   }
 }
