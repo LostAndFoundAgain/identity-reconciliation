3063792156b43ae55493c8332a783c0733e98f59
Nikhil Upadhyay <nikrkt1.com>
2025-06-21 17:18:10 +0530

    Add HTTPSTATUS.OK to request

--- src/identity/contact.controller.ts
+++ src/identity/contact.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, HttpStatus, HttpCode } from "@nestjs/common";
 import { IdentityService } from "./contact.service";
 import { CreateContactRequestDto } from "./dto/create-contact-request.dto";
 
@@ -6,6 +6,7 @@ import { CreateContactRequestDto } from "./dto/create-contact-request.dto";
 export class IdentityController {
   constructor(private readonly identityService: IdentityService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post("/identify")
   async create(@Body() createContactRequestDto: CreateContactRequestDto) {
     return await this.identityService.create(createContactRequestDto);
